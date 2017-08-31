136454a3c8c25b47faa644e3054e4e29e74de6cd
Ho Yan Leung <hy.leung@gmail.com>
2017-08-30 22:23:01 -0700

    Initial commit

---
+++ test/clj_gatling_demo/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-gatling-demo.core-test
+  (:require [clojure.test :refer :all]
+            [clj-gatling-demo.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
