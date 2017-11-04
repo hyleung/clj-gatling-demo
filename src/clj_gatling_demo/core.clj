1124679f549a7ebf271abe4315fa5b478f124e1b
Ho Yan Leung <hy.leung@gmail.com>
2017-11-03 20:18:10 -0700

    Adds load test

--- src/clj_gatling_demo/core.clj
+++ src/clj_gatling_demo/core.clj
@@ -1,7 +1,31 @@
 (ns clj-gatling-demo.core
+  (require [clj-gatling.core :as gatling]
+           [org.httpkit.client :as client]
+           )
   (:gen-class))
+
+(defn- localhost-request [_]
+  (let [{:keys [status]} @(client/get "http://localhost:8081/hello")]
+    (= status 200)))
+
+(defn- ramp-up-distribution [percentage-at _]
+  (cond
+    (< percentage-at 0.1) 0.1
+    (< percentage-at 0.2) 0.2
+    :else 1.0))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Starting run...")
+  (gatling/run
+    {:name      "Simulation"
+     :scenarios [{:name    "Localhost test scenario"
+                  :steps [
+                          {:name "Root" :request localhost-request}
+                          ]
+                  }
+                 ]
+     }
+    {:concurrency 10
+     :concurrency-distribution ramp-up-distribution}))
